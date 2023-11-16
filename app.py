48bd90644e0adefaa81d7c6768b504201521d3ce
Debayan Ghosh <debayanghosh852@gmail.com>
2023-11-16 12:08:51 +0530

    added endpoint /esp_predict

--- app.py
+++ app.py
@@ -53,6 +53,36 @@ def predict():
     
     return render_template('hello.html', prediction=result, img=crop+".png")
 
+@app.route('/esp_predict', methods=['POST'])
+def predict():
+    req=request.get_json()
+    N = int(req['Nitrogen'])
+    P = int(req['Phosporus'])
+    K = int(req['Potassium'])
+    temp = float(req['Temperature'])
+    humidity = float(req['Humidity'])
+    ph = float(req['pH'])
+    rainfall = float(req['Rainfall'])
+
+    feature_list = [N, P, K, temp, humidity, ph, rainfall]
+    single_pred = np.array(feature_list).reshape(-1,1)
+
+    prediction = loaded_model.prediction(single_pred)
+    prediction=int(prediction)
+
+    crop_dict = {1: 'rice', 2: 'maize', 3: 'chickpea', 4: 'kidneybeans', 5: 'pigeonpeas', 6: 'mothbeans',
+                 7: 'mungbean', 8: 'blackgram', 9: 'lentil', 10: 'pomegranate', 11: 'banana', 12: 'mango',
+                13: 'grapes', 14: 'watermelon', 15: 'muskmelon', 16: 'apple', 17: 'orange', 18: 'papaya',
+                19: 'coconut', 20: 'cotton', 21: 'jute', 22: 'coffee'}
+
+    if prediction in crop_dict:
+        crop = crop_dict[prediction]
+        result = "{} is the best crop to be cultivated right there".format(crop)
+    else:
+        result = "Sorry, we could not determine the best crop to be cultivated with the provided data."
+    
+    return result;
+
 @app.route('/about', methods=['GET'])
 def about():
     return render_template('about.html')
